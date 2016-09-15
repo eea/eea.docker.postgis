184e1373fb22f22981458da5c88dad152d3d7c4d
Mauro Michielon - EEA 06Oct2015 <michimau@dev-mil-24.pecs.eea>
2016-09-15 14:08:08 +0200

    refactoring and pg_hba injection via env-variable

--- postgres/sh/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-sh /create_ssl_files.sh
-
-exec /docker-entrypoint.sh "$@"
--- postgres/sh/inject_pg_hba.conf.sh
+++ postgres/sh/inject_pg_hba.conf.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -e
 
-cp /pg_hba.conf /var/lib/postgresql/data/pg_hba.conf
+rm -f /var/lib/postgresql/data/pg_hba.conf
+#cp /pg_hba.conf /var/lib/postgresql/data/pg_hba.conf
+
+echo "$PG_HBA" >> /var/lib/postgresql/data/pg_hba.conf
