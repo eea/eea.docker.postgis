532fcc4e15696f4900965449153d9e186be47140
Mauro Michielon - EEA 06Oct2015 <michimau@kvm-dev5-06.pdmz.eea>
2019-10-16 13:19:47 +0200

    removing pg_hba.conf injection

--- postgres/create_ssl_files.sh
+++ postgres/create_ssl_files.sh
@@ -10,6 +10,5 @@ cp /ssl/server.crt /var/ssl/server.crt
 cp /ssl/server.key /var/ssl/server.key
 cp /ssl/chain.crt /var/ssl/root.crt
 
+chown -R postgres.postgres /var/ssl
 chmod 600 /var/ssl/*
-
-chown postgres.postgres /var/ssl/*
--- postgres/inject_pg_hba.conf.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "injecting pg_hb.conf file"
-rm -f /var/lib/postgresql/data/pg_hba.conf
-#cp /pg_hba.conf /var/lib/postgresql/data/pg_hba.conf
-
-echo "$PG_HBA" >> /var/lib/postgresql/data/pg_hba.conf
